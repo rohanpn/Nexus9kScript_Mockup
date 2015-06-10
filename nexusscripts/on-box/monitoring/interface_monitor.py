7cf39c6a8fc8ce4124c77c6c4e7abf2127644072
rohan <rohan.nagalkar@vertisinfotech.com>
2015-06-10 21:06:13 +0530

    Update interface_monitor.py

--- nexusscripts/on-box/monitoring/interface_monitor.py
+++ nexusscripts/on-box/monitoring/interface_monitor.py
@@ -2,7 +2,7 @@
 :Product Info:Nexus::9000::9516::NX-OS Release 6.2
 :Category:Monitoring
 :Box Type:On-Box
-:Title:Interface Monitoring omnibus sdfsf
+:Title:Interface Monitoring omnibus sdfsf sdfsdf
 :Short Description:This script is to monitor Interface counters.
 :Long Description:This script is to monitor Interface counters like
 Errors, Drops, Utilization etc.
