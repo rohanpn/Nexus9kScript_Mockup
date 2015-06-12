ef467405c3b828e0e53432223fac3f94515f2114
rohan <rohan.nagalkar@vertisinfotech.com>
2015-06-12 17:38:21 +0530

    Update interface_monitor.py

--- nexusscripts/on-box/monitoring/interface_monitor.py
+++ nexusscripts/on-box/monitoring/interface_monitor.py
@@ -8,7 +8,7 @@
 Errors, Drops, Utilization etc. 
 :Input:command to check the in$terface status
      e.g show int erface ethernet 1/1
-:Output:Details of Drops,Error%and Utilization's for all the interfaces
+:Output:Details of Drops,Error%and Utilizatio'ns for all the interfaces
 """
 
 import os,sys
