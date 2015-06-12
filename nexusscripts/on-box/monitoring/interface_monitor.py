76602c675df84a1e7d4164fbea1eaf481a84ae4c
rohan <rohan.nagalkar@vertisinfotech.com>
2015-06-12 15:02:33 +0530

    Update interface_monitor.py

--- nexusscripts/on-box/monitoring/interface_monitor.py
+++ nexusscripts/on-box/monitoring/interface_monitor.py
@@ -2,7 +2,7 @@
 :Product Info:Nexus::9000::9516::NX-OS Release 6.2
 :Category:Monitoring
 :Box Type:On-Box
-:Title: Interface Monitoring d d'da'd'f as vfdgbdfg gfd gfd gsfg g dfg
+:Title: Interface Monitoring d d'da'd'f as vfdgbdfg 
 :Short Description:This script is to monitor Interfa'ce counters.
 :Long Description:This script is to monitor Interfa'ce counters like
 Errors, Drops, Utilization etc.
