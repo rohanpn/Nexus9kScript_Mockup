27cb9322a22282212fd5809a086b1761c218fc6d
rohan <rohan.nagalkar@vertisinfotech.com>
2015-06-22 16:11:51 +0530

    Update test_file.py

--- nexusscripts/on-box/config-mgmt/test_file.py
+++ nexusscripts/on-box/config-mgmt/test_file.py
@@ -2,7 +2,7 @@
 :Product Info:Nexus::9000::9516::NX-OS Release 6.2
 :Category:Config-Mgmt
 :Box Type:On-Box
-:Title: Description  dgdfgdkfg To dynamically configure interface descriptions To dynamically configure interface descriptions To dynamically configure interface descriptions To dynamically configure interface descriptions To dynamically configure interface descriptions To dynamically configure interface descriptions To dynamically configure interface descriptions
+:Title: Description  dgdfgdkfg To dynamically configure interface dconfigure interface descriptions
 :Short Description:To dynamically configure interface descriptions
 :Long Description:Check the CDP state and modify the interface description accordingly.
 :Input:command to check the CDP state and based on the command output,
