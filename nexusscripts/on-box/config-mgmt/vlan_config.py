516759aea7fbca5e68e02afe8c64a5d3de9e5d58
rohan <rohan.nagalkar@vertisinfotech.com>
2015-06-22 15:20:54 +0530

    Update vlan_config.py

--- nexusscripts/on-box/config-mgmt/vlan_config.py
+++ nexusscripts/on-box/config-mgmt/vlan_config.py
@@ -2,7 +2,7 @@
 :Product Info:Nexus::9000::9516::NX-OS Release 6.2
 :Category:Config-Mgmt
 :Box Type:On-Box
-:Title:VLAN Configuration Management
+:Title:VLAN Configuration Management sCRIPT
 :Short Description:This script is to perform VLAN operations
 :Long Description:This script is to perform configuration operations
     of VLAN interfaces
