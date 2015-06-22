73c7a0fca6a5cad21a2452a7ed51134a16e8aeb1
rohan <rohan.nagalkar@vertisinfotech.com>
2015-06-22 18:27:51 +0530

    Update test_file2.py

--- nexusscripts/on-box/config-mgmt/test_file2.py
+++ nexusscripts/on-box/config-mgmt/test_file2.py
@@ -2,7 +2,7 @@
 :Product Info:Nexus::9000::9516::NX-OS Release 6.2
 :Category:Config-Mgmt
 :Box Type:On-Box
-:Title: test2
+:Title: test2 o dynamically configure FEXo dynamically configure FEXo dynamically configure FEXo dynamically configure FEXo dynamically configure FEXo dynamically configure FEXo dynamically configure FEXo dynamically configure FEXo dynamically configure FEXo dynamically configure FEXo dynamically configure FEXo dynamically configure FEXo dynamically configure FEX
 :Short Description:To dynamically configure FEX
 :Long Description:Check the FEX state.If not installed,install the FEX.
 If not enabled ,enable the FEX.
