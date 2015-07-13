2f15c67624661b017f55789370ebf5b939cf7b0d
rohan <rohan.nagalkar@vertisinfotech.com>
2015-07-13 13:06:45 +0530

    Update kiran1.py

--- nexusscripts/on-box/config-mgmt/kiran1.py
+++ nexusscripts/on-box/config-mgmt/kiran1.py
@@ -2,7 +2,7 @@
 :Product Info:Nexus::9000::9516::NX-OS Release 6.2
 :Category:Config-Mgmt
 :Box Type:On-Box
-:Title:Kiran config managem
+:Title:Kiran config management
 :Short Description:To dynamically configure interface descriptions
 :Long Description:Check the LLDP state and modify the interface description accordingly.
 :Input:command to check the LLDP state and based on the command output,
